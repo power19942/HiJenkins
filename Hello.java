6302de95f007f827d80bb5b4c25035f672603a79
omar jakmira <poweromar19942@gmail.com>
2021-03-16 12:51:01 +0200

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
   public static void main(String[] args){
     System.out.println("hello world from jekins");
-    System.out.println("new changes");
+    System.out.println("new changes | webhook");
   }
 }
